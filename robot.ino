191795e369e97ef8fe47033ded8ffdeb23f870b0
Iván <gentoo.power@gmail.com>
2014-12-31 14:29:34 +0100

    up freq of pwm pins

--- robot.ino
+++ robot.ino
@@ -5,7 +5,7 @@
 #include <NewPing.h>
 
 #define TRIGGER_PIN  2  // Arduino pin tied to trigger pin on the ultrasonic sensor.
-#define ECHO_PIN     3  // Arduino pin tied to echo pin on the ultrasonic sensor.
+#define ECHO_PIN     12  // Arduino pin tied to echo pin on the ultrasonic sensor.
 #define MAX_DISTANCE 200 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400cm.
 
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
@@ -19,6 +19,11 @@ bool blue = false;
 
 void setup()
 {
+	//~30kHz on pwm pins 3 & 11
+	// - minimize noise
+	// - minimize heat (saturation of motor --> L293=RIP)
+	TCCR2A = (0<<WGM21) | (1<<WGM20) | (0<<COM2A1) | (0<<COM2A0) | (0<<COM2B1) | (0<<COM2B0);
+	TCCR2B = (0<<WGM22) | (0<<CS22) | (0<<CS21) | (1<<CS20) | (0<<FOC2A) | (0<<FOC2B);
 #ifdef DEBUG
 	Serial.begin(9600); // Open serial monitor at 9600 baud to see ping results.
 #endif
@@ -44,14 +49,14 @@ void loop()
 		unsigned int distance = uS / US_ROUNDTRIP_CM;
 		if (distance > 10)
 		{
-			forward(200,200);
+			forward(255,255);
 			return;
 		}
 		else if (distance < 10)
 		{
-			back(100, 100);
+			back(255, 255);
 			delay(2000);
-			left(100, 100);
+			left(255, 255);
 			delay(2000);
 		}
 	}
