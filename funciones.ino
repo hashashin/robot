cac5d5ca193feff5ce449e6e58758ed2fd327bc5
Iván <gentoo.power@gmail.com>
2015-02-22 06:27:25 +0100

    clean up and update

--- funciones.ino
+++ funciones.ino
@@ -22,91 +22,123 @@ void right(byte speed, byte speed2)
 	robotmotor.M2move(speed2, 0);
 }
 
-void stop()
+void stop(bool i)
 {
 	robotmotor.M1stop();
 	robotmotor.M2stop();
+	if (i){
+		beep(1);
+	}
 }
 
-void toggle()
+void toggle(bool i)
 {
-	if (automove){
-		automove = false;
-		stop();
+	if (i){
+		if (automove){
+			automove = false;
+			digitalWrite(LED_BUILTIN, LOW);
+			stop(1);
+		}
+		else{
+			automove = true;
+			beep(0);
+		}
 	}
-	else{
-		automove = true;
+	else if (!i)
+	{
+		if (follow){
+			follow = false;
+			digitalWrite(LED_BUILTIN, LOW);
+			stop(1);
+		}
+		else{
+			follow = true;
+			beep(0);
+		}
 	}
-
 }
 
 void BlueControl()
 {
 	byte bluedata;
-	bluedata = BT.read();
+	bluedata = Serial.read();
 	switch (bluedata)
 	{
 	case '8'://Forward
-		forward(OFFSET_SPEED, MAX_SPEED);
-		BT.println();
+		forward(speed, speed - OFFSET_SPEED);
+		Serial.println();
 		break;
 
 	case '4'://left
-		left(MAX_SPEED, MAX_SPEED);
-		BT.println();
+		left(speed, speed);
+		Serial.println();
 		break;
 
 	case '2'://bacK
-		back(OFFSET_SPEED, MAX_SPEED);
-		BT.println();
+		back(speed, speed - OFFSET_SPEED);
+		Serial.println();
 		break;
 
 	case '6'://right
-		right(MAX_SPEED, MAX_SPEED);
-		BT.println();
+		right(speed, speed);
+		Serial.println();
 		break;
 
 	case '5'://stop
-		stop();
-		BT.println();
+		stop(1);
+		Serial.println();
 		break;
 
 	case '0'://switch auto mode
-		toggle();
-		BT.println();
+		toggle(1);
+		Serial.println();
 		break;
+
+	case '9':
+		Serial.print(F("ROBOT VERSION: ")); Serial.println(RVER);
+		break;
+
+	case '1':
+		toggle(0);
+		Serial.println();
+		break;
+
+	case '3':
+		pingDebug();
+		break;
+
 #ifdef SERVOS
 	case '7':
 		if (pos >= 10){
 			myservo.write(pos -= 10);
-			BT.println();
+			Serial.println();
 			break;
 		}
-		BT.println(F("servo limit reached"));
+		Serial.println(F("servo limit reached"));
 		break;
 	case '9':
 		if (pos <= 170){
 			myservo.write(pos += 10);
-			BT.println();
+			Serial.println();
 			break;
 		}
-		BT.println(F("servo limit reached"));
+		Serial.println(F("servo limit reached"));
 		break;
 	case '1':
 		if (pos >= 10){
 			myservo2.write(pos -= 10);
-			BT.println();
+			Serial.println();
 			break;
 		}
-		BT.println(F("servo2 limit reached"));
+		Serial.println(F("servo2 limit reached"));
 		break;
 	case '3':
 		if (pos <= 170){
 			myservo2.write(pos += 10);
-			BT.println();
+			Serial.println();
 			break;
 		}
-		BT.println(F("servo2 limit reached"));
+		Serial.println(F("servo2 limit reached"));
 		break;
 #endif
 	default: return;
@@ -115,39 +147,131 @@ void BlueControl()
 
 void SonarControl()
 {
-	delay(100);
-	unsigned int distance = sonar.ping_cm(); // Send ping, get distance in cm, 0 if > MAX_DISTANCE.
-	//unsigned int distance2 = sonar2.ping_cm();
-#ifdef DEBUG
-	Serial.print(F("Ping: "));
-	Serial.print(distance);
+	unsigned int distance_L = sonar_L.convert_cm(sonar_L.ping_median()); // Send ping, get distance in cm, 0 if > MAX_DISTANCE.
+	unsigned int distance_R = sonar_R.convert_cm(sonar_R.ping_median());
+	//unsigned int distance_F = sonar_F.convert_cm(sonar_F.ping_median());
+#ifdef DEBUG_M
+	/*Serial.print(F("Ping_F: "));
+	Serial.print(distance_F);*/
 	Serial.println(F("cm"));
-	/*Serial.print(F("Ping2: "));
-	Serial.print(distance2);
-	Serial.println(F("cm"));*/
+	Serial.print(F("Ping_L: "));
+	Serial.print(distance_L);
+	Serial.println(F("cm"));
+	Serial.print(F("Ping_R: "));
+	Serial.print(distance_R);
+	Serial.println(F("cm"));
 #endif
-	if (distance < OBSTACLE && distance >= STUCK)
+	/*if (distance_F <= OBSTACLE)
 	{
-		left(MAX_SPEED, MAX_SPEED);
-	}
-	else if (distance < STUCK && distance != 0 )//&& (distance2 > OBSTACLE || distance2 == 0))
-	{
-		digitalWrite(LED_BUILTIN, HIGH);
-		back(OFFSET_SPEED, MAX_SPEED);
-		delay(1000);
-		right(0, MAX_SPEED);
-	}
-	/*else if (distance2 < OBSTACLE && distance2 >= STUCK)
-	{
-		digitalWrite(LED_BUILTIN, HIGH);
-		left(MAX_SPEED, 0);
-	}
-	else if (distance2 < STUCK && distance2 != 0)
-	{
-		right(0, MAX_SPEED);
+		stop();
+		while (sonar_F.convert_cm(sonar_F.ping_median()) <= OBSTACLE)
+		{
+			back(speed, speed);
+		}
+		return;
 	}*/
-	else {
-		digitalWrite(LED_BUILTIN, LOW);
-		forward(OFFSET_SPEED, MAX_SPEED);
+
+	if (distance_L <= OBSTACLE && distance_L > STUCK && distance_R >= OBSTACLE)
+	{
+		if (firstrun)
+		{
+			firstrun = false;
+			stop(1);
+			return;
+		}
+		tone(TONE_PIN, 700, 200);
+		digitalWrite(LED_BUILTIN, HIGH);
+		while (sonar_L.convert_cm(sonar_L.ping_median()) < OBSTACLE)
+		{
+			right(speed, speed);
+		}
+		return;
 	}
+	if (distance_R <= OBSTACLE && distance_R > STUCK && distance_L >= OBSTACLE)
+	{
+		if (firstrun)
+		{
+			firstrun = false;
+			stop(1);
+			return;
+		}
+		tone(TONE_PIN, 500, 200);
+		digitalWrite(LED_BUILTIN, HIGH);
+		while (sonar_R.convert_cm(sonar_R.ping_median()) < OBSTACLE)
+		{
+			left(speed, speed);
+		}
+		return;
+	}
+	if (distance_L <= STUCK && distance_L != 0 || distance_R <= STUCK && distance_R != 0)
+	{
+		back(speed, speed - OFFSET_SPEED);
+		delay(700);
+		return;
+	}
+	digitalWrite(LED_BUILTIN, LOW);
+	forward(speed, speed - OFFSET_SPEED);
+}
+
+void Follow()
+{
+	unsigned int distance_L = sonar_L.convert_cm(sonar_L.ping_median()); // Send ping, get distance in cm, 0 if > MAX_DISTANCE.
+	unsigned int distance_R = sonar_R.convert_cm(sonar_R.ping_median());
+#ifdef DEBUG_M
+	/*Serial.print(F("Ping_F: "));
+	Serial.print(distance_F);*/
+	Serial.println(F("cm"));
+	Serial.print(F("Ping_L: "));
+	Serial.print(distance_L);
+	Serial.println(F("cm"));
+	Serial.print(F("Ping_R: "));
+	Serial.print(distance_R);
+	Serial.println(F("cm"));
+#endif
+	if (distance_L > 18 && distance_R > 18 || distance_L < 5 && distance_R < 5) //if both sensors read more than 18 cm or less than 5 cm, brake both motors
+	{
+		stop(0);
+	}
+	if (distance_L <= 18 && distance_R <= 18 && (distance_L >= 5 && distance_R >= 5)) //if both sensors read less than 18 cm, go forward
+	{
+		forward(speed, speed - OFFSET_SPEED);
+	}
+	if (distance_L < 18 & distance_R > 18) //if only something is in front of the left ultrasonic sensor, turn left
+	{
+		left(speed, speed);
+	}
+	if (distance_L > 18 & distance_R < 18) //if only something is in front of the right ultrasonic sensor, turn right
+	{
+		right(speed, speed);
+	}
+}
+
+void beep(bool i)
+{
+	if (i)
+	{
+		tone(TONE_PIN, 700, 200);
+		delay(200);
+		tone(TONE_PIN, 1000, 200);
+	}
+	else
+	{
+		tone(TONE_PIN, 1000, 200);
+		delay(200);
+		tone(TONE_PIN, 700, 200);
+	}
+}
+
+void pingDebug()
+{
+	delay(50);
+	unsigned int d = sonar_L.convert_cm(sonar_L.ping_median()); // Send ping, get distance in cm, 0 if > MAX_DISTANCE.
+	unsigned int d2 = sonar_R.convert_cm(sonar_R.ping_median());
+	Serial.println();
+	Serial.print(F("Ping_L: "));
+	Serial.print(d);
+	Serial.println(F("cm"));
+	Serial.print(F("Ping_R: "));
+	Serial.print(d2);
+	Serial.println(F("cm"));
 }
--- robot.ino
+++ robot.ino
@@ -1,29 +1,75 @@
-//#define DEBUG
-#define SERVOS
+/*
+PIN DEFINITIONS
+---------------
+pwmPinM1 = D3
+sensePinM1 = D5
+sense2PinM1 = D6
+pwmPinM2 = D11
+sensePinM2 = D7
+sense2PinM2 = D8
+PING_PIN = D2
+ECHO_PIN = D9
+PING_PIN2 = D4
+ECHO_PIN2  = D10
+TONE_PIN = D12
+PING_PIN_F = A2 (D16)
+A0 & A1(D14, D15) = RX, TX bluetooth
+LED_PIN = A3 (D17)
+*/
+
+//#define DEBUG_M
+//#define SERVOS
+//#define I2C_T
+
+#define RVER 1
+
 #include <DualMotor.h>
-#include <SoftwareSerial.h>
+//#include <SoftwareSerial.h>
 #include <NewPing.h>
-#ifdef SERVOS
-#include <Servo.h>
+//#include <SoftTimer.h>
+//#include <Heartbeat.h>
+
+#ifdef I2C_T
+#include <I2Cdev.h>
+#include <MPU6050.h>
+#include <Wire.h>
+// Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation
+// is used in I2Cdev.h
+#define OUTPUT_READABLE_ACCELGYRO
 #endif
-#define TRIGGER_PIN  2  // Arduino pin tied to trigger pin on the ultrasonic sensor.
-#define ECHO_PIN     4  // Arduino pin tied to echo pin on the ultrasonic sensor.
+
+#ifdef SERVOS
+//#include <Servo.h>
+#endif
+
+//#define PING_PIN_F 16
+#define PING_PIN_L  2  // Arduino pin tied to trigger ping on the ultrasonic sensor.
+#define ECHO_PIN_L 9
+#define PING_PIN_R  4  // Arduino pin tied to trigger ping on the ultrasonic sensor.
+#define ECHO_PIN_R 10
 #define MAX_DISTANCE 200 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400cm.
-//#define TRIGGER_PIN2  16  // Arduino pin tied to trigger pin on the ultrasonic sensor.
-//#define ECHO_PIN2     17  // Arduino pin tied to echo pin on the ultrasonic sensor.
+
+//#define LED_PIN 17
+#define TONE_PIN 12
 
 //MOTORS && CONTROL STUFF
-#define MAX_SPEED	180
-#define OFFSET_SPEED MAX_SPEED - 25
-#define OBSTACLE 20
-#define STUCK OBSTACLE - 10
+#define MAX_SPEED 115
+#define OFFSET_SPEED 5
+#define OBSTACLE 30
+#define STUCK 10
 
-NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
-//NewPing sonar2(TRIGGER_PIN2, ECHO_PIN2, MAX_DISTANCE);
+byte speed = MAX_SPEED;
 
-SoftwareSerial BT(14, 15); //A0 y A1 // RX, TX
+//NewPing sonar_F(PING_PIN_F, PING_PIN_F, MAX_DISTANCE);
+NewPing sonar_L(PING_PIN_L, ECHO_PIN_L, MAX_DISTANCE);
+NewPing sonar_R(PING_PIN_R, ECHO_PIN_R, MAX_DISTANCE);
+
+//SoftwareSerial BT(14, 15); //A0 y A1 // RX, TX
 
 DualMotor robotmotor;
+
+//Task task1(100, control);
+//Heartbeat heartbeat(LED_PIN);
 
 #ifdef SERVOS
 Servo myservo;
@@ -32,38 +78,53 @@ byte pos = 90;
 byte pos2 = 90;
 bool servos = false;
 #endif
-bool automove = false;
 
+#ifdef I2C_T
+MPU6050 mpu;
+#endif
+
+bool automove = false;
+bool firstrun = true;
+bool follow = false;
 
 void setup()
 {
 	//~30Hz on pwm pins 3 & 11
 	// - minimize noise
 	// - minimize heat (saturation of motor --> L293=RIP)
-	TCCR2B = TCCR2B & (B11111000 | B00000111);
+	TCCR2B = TCCR2B & B11111000 | B00000111;
 	pinMode(LED_BUILTIN, OUTPUT);
-#ifdef DEBUG
-	Serial.begin(9600); // Open serial monitor at 9600 baud to see ping results.
+#ifdef DEBUG_M
+	//Serial.begin(9600); // Open serial monitor at 9600 baud to see ping results.
 #endif
-	BT.begin(9600);
+	Serial.begin(115200);
 #ifdef SERVOS
 	myservo.attach(9);
 	myservo2.attach(10);
 	myservo.write(pos);
 	myservo2.write(pos2);
 #endif
-	stop();
+#ifdef I2C_T
+	mpu.initialize();
+	mpu.testConnection();
+#endif
+	stop(1);
+	//SoftTimer.add(&task1);
+	beep(1);
 }
 
 void loop()
 {
-	if (BT.available() > 0)
+	if (Serial.available() > 0)
 	{
 		BlueControl();
 	}
-	else if (automove == true)
+	else if (automove)
 	{
 		SonarControl();
 	}
+	else if (follow && !automove)
+	{
+		Follow();
+	}
 }
-
